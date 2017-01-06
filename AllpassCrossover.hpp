1d0a8e377b801901850a23fbcc024c8754f130a5
Raphael Wettinger <raphael@w.org>
2017-01-06 01:50:54 +0100

    xover: comments, todos & small actor param change

--- AllpassCrossover.hpp
+++ AllpassCrossover.hpp
@@ -26,130 +26,6 @@ struct AllpassState {
     //------------------------------
     AllpassState():q_nm1(0), p_nm1(0) {}
 };
-
-   
-    
-// stateless (does not own delays) 2nd order allpass, transposed direct form II
-// usage: init, tune, process
-//template <typename T>
-//class AllpassFilterStateless {
-//public:
-//    // coefficients
-//    T a1;
-//    T a2;
-//    
-//    // beta (squared magnitude of the purely imaginary pole
-//    // of the startup half-band filter)
-//    T beta;
-//    
-//    // switch between 1st and 2nd order allpass
-//    bool isBiquad;
-//    
-//    //------------------------------
-//    
-//    // setBeta, setBiquad
-//    // init() ?
-//    
-//    AllpassFilterStateless():a1(0),a2(0),beta(0),isBiquad(true) {}
-//    
-//    inline void init(bool isBiquad, T beta = T(0)) {
-//        // reset coefficients
-//        a1 = T(0);
-//        a2 = T(0);
-//        
-//        this->beta = beta;
-//        this->isBiquad = isBiquad;
-//        // IDEA: tune here to half-band
-//    }
-//    
-//    inline void __tuneCrossoverFrequency(T alpha_1, T alpha) {
-//        if (isBiquad) {
-//            T beta_i = (beta + alpha_1*alpha_1) / (beta*alpha_1*alpha_1 + T(1));
-//            a1 = alpha * (T(1) + beta_i);
-//            a2 = beta_i;
-//        } else {
-//            a1 = alpha_1;
-//        }
-//    }
-//    
-//    inline void _tuneCrossoverFrequency(T frequency, T& _alpha_1, T& _alpha) {
-//        T f_3dB = (T(1)-frequency)/T(2); // mirror and scale
-//        
-//        T alpha;
-//        
-//        T tang = std::tan(M_PI*f_3dB);
-//        T alpha_1 = (T(1)-tang) / (T(1)+tang);
-//        if (isBiquad) {
-//            alpha = (T(1)-(tang*tang)) / (T(1)+(tang*tang));
-//        }
-//        
-//        __tuneCrossoverFrequency(alpha_1, alpha);
-//        
-//        _alpha_1 = alpha_1;
-//        _alpha   = alpha;
-//    }
-//    
-//    // Tune crossover frequency (0=DC, 1=Nyquist)
-//    inline void tuneCrossoverFrequency(T frequency) {
-//        T alpha_1;
-//        T alpha;
-//        _tuneCrossoverFrequency(frequency, alpha_1, alpha);
-//    }
-//
-//    
-//    inline void process(int numSamples, T* input, T* output, AllpassState<T>* state) {
-//        T q_nm1 = state->q_nm1;
-//        T p_nm1 = state->p_nm1;
-//        
-//        for (int i=0; i<numSamples; i++) {
-//            T x = input[i];
-//            
-//            T y;
-//            if (isBiquad) {
-//                // for allpass case: b0 = a2; b1 = a1; b2 = a0 = 1
-//                y = q_nm1 + a2*x;
-//                q_nm1 = p_nm1 + a1*x - a1*y;
-//                p_nm1 = x - a2*y;
-//            } else {
-//                // 1st order
-//                // for allpass case: b0 = a1; b1 = a0 = 1;
-//                y = q_nm1 + a1*x;
-//                q_nm1 = x - a1*y;
-//            }
-//            
-//            output[i] = y;
-//        }
-//        
-//        state->q_nm1 = q_nm1;
-//        state->p_nm1 = p_nm1;
-//    }
-//    
-//    inline void processHB(T* input, T* output, int numSamples, AllpassState<T>* state) {
-//        T q_nm1 = state->q_nm1;
-//        T p_nm1 = state->p_nm1;
-//        
-//        for (int i=0; i<numSamples; i++) {
-//            T x = input[i];
-//            
-//            T y;
-//            if (isBiquad) {
-//                // for allpass/HB case: b0 = a2 = beta; b1 = a1 = 0; b2 = a0 = 1
-//                y = q_nm1 + beta*x;
-//                q_nm1 = p_nm1;
-//                p_nm1 = x - beta*y;
-//            } else {
-//                // pure Delay
-//                y = q_nm1;
-//                q_nm1 = x;
-//            }
-//
-//            output[i] = y;
-//        }
-//        
-//        state->q_nm1 = q_nm1;
-//        state->p_nm1 = p_nm1;
-//    }
-//};
     
 
 // multichannel (statefull) 2nd order allpass, transposed direct form II
@@ -294,27 +170,26 @@ public:
     }
 };
 
+
     
-
-// crossover owns state, which is allocated outside
-// setFilters(AllpassFilterStateless*, int numFilterPerChannel)
-// setChannels(AllpassState*, int numChannels)
-
-// setEMQFHalfbandFilter(T* betas, int numPairs) // distribute poles and reset states
-// tuneCrossoverFrequency(T f) // iter and tune
-
-// process(int channel, int numSamples, in, outLow, outHigh)
-// processHB(int channel, int numSamples, in, outLow, outHigh)
-// processPhase(int channel, int numSamples, in, out)
+    
+// DONE:
+    // verify pole distribution (both cases, numPair odd&even) in debugging mode
+    // ? verify impulse response & proper processing
+    // ? verify tuning formula ?
 
 // TODO:
-    // verify pole distribution (both cases, numPair odd&even) in debugging mode
-    // ? verify tuning formula ?
-    
     // implement rest of methods: minimum processPhase()
+    // automatic retuning after resetting the startup half-band filter (frequency as a member)
+        // auto state reset ?
     
-// crossover owns state, which is allocated outside
-// no bounds checking is done by this class, make shure you allocated engough states & filters
+    // nicer code: conflate e.g. 1st & 2nd section in process method
+        // also 1st & 2nd section switching setEMQFHalfbandFilter()
+
+    
+// crossover owns state
+// no bounds checking is done by this class, make shure you allocated engough filters
+    // a crossover initialized with numMaxFiltersPerChannel
 
 // the xover needs:
 // - filter instances: (numPair+1)=numFiltersPerChannel
@@ -333,8 +208,10 @@ class Crossover {
     //int numChannels; // filters[0].size()
     int offset; // offset to the second filter within the channel
 public:
-    Crossover(int numChannels, int numMaxFiltersPerChannel)
-        : filters(numMaxFiltersPerChannel, AllpassFilterMultichannel<T>(numChannels) )
+    // numChannels : number of available channels
+    // numPairs    : maximum order N = (numPairs*2+1) an instance is able to process
+    Crossover(int numChannels, int numPairs)
+        : filters(numPairs+1, AllpassFilterMultichannel<T>(numChannels) )
         , offset(0)
         , numFiltersPerChannel(0)
     {
